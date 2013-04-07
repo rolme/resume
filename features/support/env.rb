b97b177104a12a43ebf9854b3fa4d7ce8b70cb53
rolme <roland.parnaso@gmail.com>
2013-04-07 16:26:16 -0700

    removed the usage of awesome_print in :test

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,6 @@
 # files.
 
 require 'cucumber/rails'
-require 'awesome_print'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
