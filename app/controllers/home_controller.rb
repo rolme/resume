95538a074d450b985c421633610717f92bc11c45
rolme <roland.parnaso@gmail.com>
2013-05-15 20:26:04 -0700

    Clean up cvs, sections association.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
--- app/models/cvs_sections.rb
+++
@@ -1,4 +0,0 @@
-class CvsSections < ActiveRecord::Base
-  belongs_to :cv
-  belongs_to :section
-end
