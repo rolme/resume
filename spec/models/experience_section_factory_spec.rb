74291b22b8a8c000d034f93c8bcbb53246c9b896
rolme <roland.parnaso@gmail.com>
2013-05-19 12:55:26 -0700

    Remove specs for factories as this is no longer needed.

--- spec/models/experience_section_factory_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe ExperienceSectionFactory do
-  my_config = [ { :TITLE => 'test1' }, { :TITLE => 'test2', :DEPARTMENT => 'department2' }, { :TITLE => 'test3' } ]
-
-  it 'returns a valid experience section' do
-    expect(ExperienceSectionFactory.build(config: my_config)[1].department).to eql 'department2'
-  end
-end
